fea387d571d8b3e9e069daa9fceb00bb1beeee2c
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-10 19:57:16 -0500

    Background and tiles javascript logic.

--- public/js/main.js
+++ public/js/main.js
@@ -14,5 +14,5 @@ const context = canvas.getContext("2d");
 context.fillRect(0, 0, 50, 50);
 
 loadImage("./img/tiles.png").then((image) => {
-  context.drawImage(image, 0, 0);
+  context.drawImage(image, 0, 0, 16, 16, 32, 32, 16, 16);
 });
