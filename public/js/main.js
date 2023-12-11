60c9db39eb869dc28538402e891c88d63b2e8681
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-10 22:21:40 -0500

    Sprites and layers JavaScript logic.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from "./SpriteSheet.js";
-import { loadImage, loadLevel } from "./loaders.js";
+import { loadLevel } from "./loaders.js";
+import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
 
 function drawBackground(background, context, sprites) {
   background.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -14,14 +14,14 @@ function drawBackground(background, context, sprites) {
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-loadImage("./img/tiles.png").then((image) => {
-  const sprites = new SpriteSheet(image, 16, 16);
-  sprites.define("ground", 0, 0);
-  sprites.define("sky", 3, 23);
-
-  loadLevel("1-1").then((level) => {
-    level.backgrounds.forEach((background) => {
-      drawBackground(background, context, sprites);
-    });
+Promise.all([
+  loadMarioSprite(),
+  loadBackgroundSprites(),
+  loadLevel("1-1"),
+]).then(([marioSprite, sprites, level]) => {
+  level.backgrounds.forEach((background) => {
+    drawBackground(background, context, sprites);
   });
+
+  marioSprite.draw("idle", context, 64, 64);
 });
