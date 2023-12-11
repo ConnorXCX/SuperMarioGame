5cebdbfd4306a28e6a3ac8109375ca102e28ad3d
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-11 18:17:38 -0500

    Add tile collision logic.

--- public/js/math.js
+++ public/js/math.js
@@ -1,3 +1,30 @@
+// const matrix = new Matrix();
+
+// matrix.set(5, 4, { name: "ground" });
+
+// const tile = matrix.get(mario.pos.x * TILE_SIZE, mario.pos.y * TILE_SIZE);
+
+export class Matrix {
+  constructor() {
+    this.grid = {};
+  }
+
+  get(x, y) {
+    const col = this.grid[x];
+    if (col) {
+      return col[y];
+    }
+    return undefined;
+  }
+
+  set(x, y, value) {
+    if (!this.grid[x]) {
+      this.grid[x] = [];
+    }
+    this.grid[x][y] = value;
+  }
+}
+
 export class Vector {
   constructor(x, y) {
     this.set(x, y);
