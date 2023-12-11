56c0f607e3897b7c5d303b025fbeefb8dddea7dd
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-11 00:08:16 -0500

    Add timing accuracy logic.

--- public/js/main.js
+++ public/js/main.js
@@ -1,55 +1,34 @@
 import Compositor from "./Compositor.js";
-import Entity from "./Entity.js";
 import { loadLevel } from "./loaders.js";
-import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer } from "./layers.js";
+import { createMario } from "./entities.js";
+import { loadBackgroundSprites } from "./sprites.js";
+import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function createSpriteLayer(entity) {
-  return function drawSpriteLayer(context) {
-    entity.draw(context);
-  };
-}
+Promise.all([createMario(), loadBackgroundSprites(), loadLevel("1-1")]).then(
+  ([mario, backgroundSprites, level]) => {
+    const compositor = new Compositor();
 
-Promise.all([
-  loadMarioSprite(),
-  loadBackgroundSprites(),
-  loadLevel("1-1"),
-]).then(([marioSprite, backgroundSprites, level]) => {
-  const compositor = new Compositor();
+    const backgroundLayer = createBackgroundLayer(
+      level.backgrounds,
+      backgroundSprites
+    );
+    compositor.layers.push(backgroundLayer);
 
-  const backgroundLayer = createBackgroundLayer(
-    level.backgrounds,
-    backgroundSprites
-  );
-  compositor.layers.push(backgroundLayer);
+    const gravity = 0.5;
 
-  const gravity = 0.5;
+    const spriteLayer = createSpriteLayer(mario);
+    compositor.layers.push(spriteLayer);
 
-  const mario = new Entity();
-  mario.position.set(64, 180);
-  mario.velocity.set(2, -10);
+    function update() {
+      compositor.draw(context);
+      mario.update();
+      mario.velocity.y += gravity;
+      requestAnimationFrame(update);
+    }
 
-  mario.draw = function drawMario(context) {
-    marioSprite.draw("idle", context, this.position.x, this.position.y);
-  };
-
-  mario.update = function updateMario() {
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  };
-
-  const spriteLayer = createSpriteLayer(mario);
-  compositor.layers.push(spriteLayer);
-
-  function update() {
-    compositor.draw(context);
-    mario.update();
-    mario.velocity.y += gravity;
-    requestAnimationFrame(update);
+    update();
   }
-
-  update();
-});
+);
