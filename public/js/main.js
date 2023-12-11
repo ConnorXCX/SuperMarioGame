be8eedf29395e13c50267289c363e01493f90adf
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-11 17:39:27 -0500

    Add tile collision logic.

--- public/js/main.js
+++ public/js/main.js
@@ -1,47 +1,33 @@
-import Compositor from "./Compositor.js";
 import Timer from "./Timer.js";
 import Keyboard from "./KeyboardState.js";
 import { loadLevel } from "./loaders.js";
 import { createMario } from "./entities.js";
-import { loadBackgroundSprites } from "./sprites.js";
-import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-Promise.all([createMario(), loadBackgroundSprites(), loadLevel("1-1")]).then(
-  ([mario, backgroundSprites, level]) => {
-    const compositor = new Compositor();
+Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
+  const gravity = 2000;
+  mario.pos.set(64, 64);
 
-    const backgroundLayer = createBackgroundLayer(
-      level.backgrounds,
-      backgroundSprites
-    );
-    compositor.layers.push(backgroundLayer);
+  level.entities.add(mario);
 
-    const gravity = 2000;
-    mario.pos.set(64, 180);
+  const SPACE = 32;
+  const input = new Keyboard();
+  input.addMapping(32, (keyState) => {
+    if (keyState) {
+      mario.jump.start();
+    } else {
+      mario.jump.cancel();
+    }
+  });
+  input.listenTo(window);
 
-    const SPACE = 32;
-    const input = new Keyboard();
-    input.addMapping(32, (keyState) => {
-      if (keyState) {
-        mario.jump.start();
-      } else {
-        mario.jump.cancel();
-      }
-    });
-    input.listenTo(window);
-
-    const spriteLayer = createSpriteLayer(mario);
-    compositor.layers.push(spriteLayer);
-
-    const timer = new Timer(1 / 60);
-    timer.update = function update(deltaTime) {
-      mario.update(deltaTime);
-      compositor.draw(context);
-      mario.vel.y += gravity * deltaTime;
-    };
-    timer.start();
-  }
-);
+  const timer = new Timer(1 / 60);
+  timer.update = function update(deltaTime) {
+    mario.update(deltaTime);
+    level.comp.draw(context);
+    mario.vel.y += gravity * deltaTime;
+  };
+  timer.start();
+});
