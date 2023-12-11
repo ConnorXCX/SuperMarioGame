b88acbec10fa3aa2714e56f1b7591a6eed64b1b4
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-10 23:47:57 -0500

    Add timing accuracy logic.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 import Compositor from "./Compositor.js";
+import Entity from "./Entity.js";
 import { loadLevel } from "./loaders.js";
 import { loadMarioSprite, loadBackgroundSprites } from "./sprites.js";
 import { createBackgroundLayer } from "./layers.js";
@@ -6,11 +7,9 @@ import { createBackgroundLayer } from "./layers.js";
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-function createSpriteLayer(sprite, pos) {
+function createSpriteLayer(entity) {
   return function drawSpriteLayer(context) {
-    for (let i = 0; i < 20; ++i) {
-      sprite.draw("idle", context, pos.x + i * 16, pos.y);
-    }
+    entity.draw(context);
   };
 }
 
@@ -27,18 +26,28 @@ Promise.all([
   );
   compositor.layers.push(backgroundLayer);
 
-  const position = {
-    x: 0,
-    y: 0,
+  const gravity = 0.5;
+
+  const mario = new Entity();
+  mario.position.set(64, 180);
+  mario.velocity.set(2, -10);
+
+  mario.draw = function drawMario(context) {
+    marioSprite.draw("idle", context, this.position.x, this.position.y);
   };
 
-  const spriteLayer = createSpriteLayer(marioSprite, position);
+  mario.update = function updateMario() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  };
+
+  const spriteLayer = createSpriteLayer(mario);
   compositor.layers.push(spriteLayer);
 
   function update() {
     compositor.draw(context);
-    position.x += 2;
-    position.y += 2;
+    mario.update();
+    mario.velocity.y += gravity;
     requestAnimationFrame(update);
   }
 
