1cf4e6a26733beeb6f1b8a999339c5f38cfa02a7
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-11 20:25:12 -0500

    Add tile collision logic.

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ import Timer from "./Timer.js";
 import Keyboard from "./KeyboardState.js";
 import { loadLevel } from "./loaders.js";
 import { createMario } from "./entities.js";
+import { createCollisionLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
@@ -9,6 +10,8 @@ const context = canvas.getContext("2d");
 Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
   const gravity = 2000;
   mario.pos.set(64, 64);
+
+  createCollisionLayer(level);
 
   level.entities.add(mario);
 
