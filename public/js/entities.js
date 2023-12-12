8874855e388baf36423b76d3ca19bddde748d4ce
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-12 15:31:10 -0500

    Add animation logic.

--- public/js/entities.js
+++ public/js/entities.js
@@ -1,15 +1,19 @@
 import Entity from "./Entity.js";
 import Jump from "./traits/Jump.js";
 import Go from "./traits/Go.js";
-import { loadMarioSprite } from "./sprites.js";
+import { loadSpriteSheet } from "./loaders.js";
 
 export function createMario() {
-  return loadMarioSprite().then((sprites) => {
+  return loadSpriteSheet("mario").then((sprites) => {
     const mario = new Entity();
     mario.size.set(14, 16);
 
     mario.addTrait(new Jump());
     mario.addTrait(new Go());
+
+    function routeFrame(mario) {
+      return "idle";
+    }
 
     mario.draw = function drawMario(context) {
       sprites.draw("idle", context, 0, 0);
