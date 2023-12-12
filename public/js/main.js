90f9013526bce40695c08be48c8123213b2400d4
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-12 03:14:09 -0500

    Add camera and scrolling logic.

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ import { loadLevel } from "./loaders.js";
 import { createMario } from "./entities.js";
 import { setupKeyboard } from "./input.js";
 import { setupMouseControl } from "./debug.js";
+import { createCameraLayer } from "./layers.js";
 
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
@@ -12,6 +13,8 @@ Promise.all([createMario(), loadLevel("1-1")]).then(([mario, level]) => {
   const camera = new Camera();
 
   mario.pos.set(64, 64);
+
+  level.comp.layers.push(createCameraLayer(camera));
 
   level.entities.add(mario);
 
