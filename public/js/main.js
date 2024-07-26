c21be9654de81bb13e12a78d432cba1a55688c8d
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-07-25 21:28:16 -0400

    Refactor loading of entities in main with a new add load entity factory method.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,7 @@
 import Timer from "./Timer.js";
 import Camera from "./Camera.js";
 import { loadLevel } from "./loaders/level.js";
-import { loadMario } from "./entities/Mario.js";
-import { loadGoomba } from "./entities/Goomba.js";
-import { loadKoopa } from "./entities/Koopa.js";
+import { loadEntities } from "./entities.js";
 import { setupKeyboard } from "./input.js";
 import { setupMouseControl } from "./debug.js";
 import { createCameraLayer, createCollisionLayer } from "./layers.js";
@@ -11,47 +9,45 @@ import { createCameraLayer, createCollisionLayer } from "./layers.js";
 const canvas = document.getElementById("screen");
 const context = canvas.getContext("2d");
 
-Promise.all([loadMario(), loadGoomba(), loadKoopa(), loadLevel("1-1")]).then(
-  ([createMario, createGoomba, createKoopa, level]) => {
-    const camera = new Camera();
-    // window.camera = camera;
+Promise.all([loadEntities(), loadLevel("1-1")]).then(([entity, level]) => {
+  const camera = new Camera();
+  // window.camera = camera;
 
-    const mario = createMario();
+  const mario = entity.mario();
 
-    mario.pos.set(64, 64);
+  mario.pos.set(64, 64);
 
-    const goomba = createGoomba();
-    goomba.pos.x = 220;
-    level.entities.add(goomba);
+  const goomba = entity.goomba();
+  goomba.pos.x = 220;
+  level.entities.add(goomba);
 
-    const koopa = createKoopa();
-    koopa.pos.x = 260;
-    level.entities.add(koopa);
+  const koopa = entity.koopa();
+  koopa.pos.x = 260;
+  level.entities.add(koopa);
 
-    // Debug camera border.
-    level.comp.layers.push(createCameraLayer(camera));
+  // Debug camera border.
+  level.comp.layers.push(createCameraLayer(camera));
 
-    level.entities.add(mario);
+  level.entities.add(mario);
 
-    level.comp.layers.push(createCollisionLayer(level));
+  level.comp.layers.push(createCollisionLayer(level));
 
-    const input = setupKeyboard(mario);
+  const input = setupKeyboard(mario);
 
-    input.listenTo(window);
+  input.listenTo(window);
 
-    // Debug mouse control.
-    setupMouseControl(canvas, mario, camera);
+  // Debug mouse control.
+  setupMouseControl(canvas, mario, camera);
 
-    const timer = new Timer(1 / 60);
-    timer.update = function update(deltaTime) {
-      level.update(deltaTime);
+  const timer = new Timer(1 / 60);
+  timer.update = function update(deltaTime) {
+    level.update(deltaTime);
 
-      if (mario.pos.x > 100) {
-        camera.pos.x = mario.pos.x - 100;
-      }
+    if (mario.pos.x > 100) {
+      camera.pos.x = mario.pos.x - 100;
+    }
 
-      level.comp.draw(context, camera);
-    };
-    timer.start();
-  }
-);
+    level.comp.draw(context, camera);
+  };
+  timer.start();
+});
